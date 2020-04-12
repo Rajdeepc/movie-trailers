8d72fcede4ec5fc7a782e60f74afbbacdcd720e7
Rajdeepc <rajrock38@gmail.com>
2020-04-12 23:53:00 +0530

    added css

--- src/components/gridItemDesc.js
+++ src/components/gridItemDesc.js
@@ -12,85 +12,94 @@ export const GridItemDesc = (props) => {
   const genreSeparator = itemDetails.EventGenre.split("|");
   const splitDate = itemDetails ? itemDetails.ShowDate.split(",") : "";
 
-
   const closeItem = () => {
-      props.onCloseGridItem(true)
-  }
+    props.onCloseGridItem(true);
+  };
   return (
     <div className="griddescwrapper">
       <div className="columns is-mobile">
-        <div className="column is-three-quarters-mobile is-two-thirds-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd">
+        <div className="column">
           <iframe
             width="100%"
             height="315"
-            frameborder="0" allowfullscreen
+            frameborder="0"
+            allowfullscreen
             title={itemDetails.EventTitle}
             src={itemDetails.TrailerURL.replace("watch?v=", "embed/")}
           ></iframe>
         </div>
-        <div className="column is-three-quarters-mobile is-two-thirds-tablet is-half-desktop is-one-third-widescreen is-one-quarter-fullhd videodetail">
+        <div className="column">
           <div className="videoDetails">
-            <div className="videoDetailsrow">
-            <h4>
-              <b>{itemDetails.EventName}</b>
-            </h4>
-            <a href="#"  className="close" onClick={closeItem}><MdClose/></a>
-            </div>
-            
-            <p className="language">{itemDetails.EventLanguage}</p>
-            <div className="tags noMargin">
-              {genreSeparator.map((item) => {
-                return <span className="tag">{item}</span>;
-              })}
-            </div>
-            <div className="columns is-gapless is-mobile">
-              <div className="column is-2">
-                <div className="voting">
-                  <i className="fas fa-thumbs-up" />
-                  <span>{itemDetails.wtsPerc} %</span>
-                  <div className="lowerItem">{itemDetails.wtsCount} votes</div>
-                </div>
+            <div className="upper">
+              <div className="videoDetailsrow">
+                <h4>
+                  <b>{itemDetails.EventName}</b>
+                </h4>
+                <a href="#" className="close" onClick={closeItem}>
+                  <MdClose />
+                </a>
+                
               </div>
-              <div className="column is-10">
-                <div className="release">
-                  <i className="fas fa-calendar-alt" />
-                  <span>{splitDate[0]}</span>
-                  <div className="lowerItem">{splitDate[1]}</div>
-                </div>
+              <p className="language">{itemDetails.EventLanguage}</p>
+              <div className="tags noMargin">
+                {genreSeparator.map((item) => {
+                  return <span className="tag">{item}</span>;
+                })}
               </div>
-            </div>
-          </div>
-          <div className="ratingVotes columns is-mobile">
-            <div className="column is-4 is-vertical-center text-center">
-              <div className="liked">
-                <div className="text-center">
-                  <i className="far fa-thumbs-up"></i>
+              <div className="columns is-gapless">
+                <div className="column is-2">
+                  <div className="voting">
+                    <i className="fas fa-thumbs-up" />
+                    <span>{itemDetails.wtsPerc} %</span>
+                    <div className="lowerItem">
+                      {itemDetails.wtsCount} votes
+                    </div>
+                  </div>
                 </div>
-                <div className="totalwtached greenTxt">
-                  <p>WILL WATCH</p>
-                  <p>( {itemDetails.wtsCount} )</p>
+                <div className="column is-10">
+                  <div className="release">
+                    <i className="fas fa-calendar-alt" />
+                    <span>{splitDate[0]}</span>
+                    <div className="lowerItem">{splitDate[1]}</div>
+                  </div>
                 </div>
               </div>
             </div>
-            <div className="column is-vertical-center is-4 text-center">
-              <div className="maybe">
-                <div className="text-center">
-                  <i className="fas fa-question"></i>
+
+            <div className="lower">
+              <div className="ratingVotes columns">
+                <div className="column is-4 is-vertical-center text-center">
+                  <div className="liked">
+                    <div className="text-center">
+                      <i className="far fa-thumbs-up"></i>
+                    </div>
+                    <div className="totalwtached greenTxt">
+                      <p>WILL WATCH</p>
+                      <p>( {itemDetails.wtsCount} )</p>
+                    </div>
+                  </div>
                 </div>
-                <div className="totalwtached orangeTxt">
-                  <p>MAYBE</p>
-                  <p>( {itemDetails.maybeCount} )</p>
+                <div className="column is-vertical-center is-4 text-center">
+                  <div className="maybe">
+                    <div className="text-center">
+                      <i className="fas fa-question"></i>
+                    </div>
+                    <div className="totalwtached orangeTxt">
+                      <p>MAYBE</p>
+                      <p>( {itemDetails.maybeCount} )</p>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
-            <div className="column is-4 is-vertical-center text-center">
-              <div className="dontlike">
-                <div className="text-center">
-                  <i className="far fa-thumbs-down"></i>
-                </div>
-                <div className="totalwtached red">
-                  <p>WONT WATCH</p>
-                  <p>( {itemDetails.dwtsCount} )</p>
+                <div className="column is-4 is-vertical-center text-center">
+                  <div className="dontlike">
+                    <div className="text-center">
+                      <i className="far fa-thumbs-down"></i>
+                    </div>
+                    <div className="totalwtached red">
+                      <p>WONT WATCH</p>
+                      <p>( {itemDetails.dwtsCount} )</p>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
